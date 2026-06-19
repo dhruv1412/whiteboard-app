62c52a1cad0f9808f36faf928af4b455395ab38f
Devendra Pratap Singh <devendraa_01@gmail.com>
2026-06-19 16:06:40 +0530

    fixing bugs

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -15,6 +15,7 @@ const transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
     port: 587,
     secure: false, // Must be false for port 587
+    localAddress: '0.0.0.0',
     auth: { 
         user: process.env.EMAIL_USER, 
         pass: process.env.EMAIL_PASS 
